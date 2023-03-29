9b4fa0da00f125c2ba8c6baeb7a243c86e47484f
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2023-03-30 00:13:59 +0530

    final commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log('js')
+
 let add = document.getElementById('add')
 add.addEventListener('click',addItem)
 
@@ -8,28 +8,36 @@ remul.addEventListener('click',removeItem)
 let complete = document.getElementById('done')
 complete.addEventListener('click',removeItem)
 
+function addInDom(item){
+    let ul = document.getElementById('remaining')
+        let li = document.createElement('li')
+        li.appendChild(document.createTextNode(`${item.todoName}  ${item.todoDes}`))
+        let doneBtn = document.createElement('button')
+        let delBtn = document.createElement('button')
+        doneBtn.appendChild(document.createTextNode('✓'))
+        delBtn.appendChild(document.createTextNode('x'))
+        doneBtn.setAttribute('id',`${item._id}`)
+        delBtn.setAttribute('id',`${item._id}`)
+        delBtn.setAttribute('class','delbtn float-right m-1')
+        doneBtn.setAttribute('class','done float-right m-1')
+        li.appendChild(delBtn)
+        if(!item.isDone){
+        li.appendChild(doneBtn)
+        ul.appendChild(li)
+        }
+        else{
+            let ul = document.getElementById('done')
+            ul.appendChild(li)
+        }
+}
+
 function addItem(e){
     e.preventDefault();
     let todoName = document.getElementById('todoName').value;
     let todoDes = document.getElementById('todoDes').value;
-    console.log('submitted');
-    console.log(todoName,todoDes)
-    let ul = document.getElementById('remaining')
-    let li = document.createElement('li')
-    li.appendChild(document.createTextNode(`${todoName}  ${todoDes}`))
-    let doneBtn = document.createElement('button')
-    let delBtn = document.createElement('button')
-    doneBtn.appendChild(document.createTextNode('✓'))
-    delBtn.appendChild(document.createTextNode('x'))
-    doneBtn.setAttribute('id','done')
-    delBtn.setAttribute('id','delete')
-    doneBtn.setAttribute('style',' margin-left:70px')
-    delBtn.setAttribute('style',' margin-left:10px')
-    li.appendChild(doneBtn)
-    li.appendChild(delBtn)
-    ul.appendChild(li)
     let objData = {todoName,todoDes,isDone:false}
     addInCrud(objData)
+    addInDom(objData)
     document.getElementById('todoName').value=''
     document.getElementById('todoDes').value=''
 }
@@ -38,55 +46,34 @@ function addItem(e){
 
 let printTodo = async () =>{
   let todos = await getFromCrud()
-   todos.map((item) =>{
-        let ul = document.getElementById('remaining')
-        let li = document.createElement('li')
-        li.appendChild(document.createTextNode(`${item.todoName}  ${item.todoDes}`))
-        let doneBtn = document.createElement('button')
-        let delBtn = document.createElement('button')
-        doneBtn.appendChild(document.createTextNode('✓'))
-        delBtn.appendChild(document.createTextNode('x'))
-        doneBtn.setAttribute('id','done')
-        delBtn.setAttribute('id','delete')
-        delBtn.setAttribute('class',`${item._id}`)
-        doneBtn.setAttribute('style',' margin-left:100px')
-        delBtn.setAttribute('style',' margin-left:10px')
-        li.appendChild(doneBtn)
-        li.appendChild(delBtn)
-        ul.appendChild(li)
-
-      //}
-   })
+   todos.map((item) =>addInDom(item))
 }
 window.addEventListener('DOMContentLoaded',printTodo)
+
 function doneFun(e){
-    if(e.target.getAttribute('id') == 'done')
+    if(e.target.getAttribute('class') == 'done float-right m-1')
     {
         let curNode = e.target.parentElement;
          let data = curNode.textContent.split('  ')
+         let id = e.target.getAttribute('id')
+         console.log('doneid',id)
          console.log('data--',data)
          console.log(e.target.getAttribute('class'))
          let li = document.createElement('li')
-         li.appendChild(document.createTextNode(`${data[0]}  ${data[1].slice(0,-2)}`))
-        // let delBtn = document.createElement('button')
-        // delBtn.appendChild(document.createTextNode('x'))
-        // delBtn.setAttribute('id','delete')
-        // delBtn.setAttribute('style',' margin-left:60px')
-        // li.appendChild(delBtn)
-        curNode.remove()
-         let doneul = document.getElementById('done')   
-         doneul.appendChild(li);
-        // console.log(doneul)
-         
-       // curNode.remove();
+         let todoName = data[0]
+         let todoDes = data[1].slice(0,-2)
+         let objData = {todoName,todoDes,isDone:true}
+         curNode.remove()
+         addInDom(objData)
+         updateCrud(objData,id)
     }
 }
 
 function removeItem(e){
-    if(e.target.getAttribute('id') == 'delete'){
+    if(e.target.getAttribute('class') == 'delbtn float-right m-1'){
         let curNode = e.target.parentElement
         curNode.remove()
-        let id = e.target.getAttribute('class')
+        let id = e.target.getAttribute('id')
         if(id)
        axios.delete(`https://crudcrud.com/api/956eed358c134b8880eef7674818b07d/todo/${id}`)
     }
@@ -109,4 +96,10 @@ let getFromCrud = async () =>{
     catch(err){
         console.log(err)
     }
+}
+
+let updateCrud = (todoItem,id) =>{
+   axios.put(`https://crudcrud.com/api/956eed358c134b8880eef7674818b07d/todo/${id}`,todoItem)
+   .then(res => console.log(res))
+   .catch(err => console.log(err))
 }
